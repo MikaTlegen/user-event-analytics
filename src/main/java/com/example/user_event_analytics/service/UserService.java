a2cd9f78e65d36b4a9584ee6acd03bcb8470d681
tlege <tlegermuh88@gmail.com>
2026-02-11 15:10:47 +0300

    implemented a Criteria method

--- src/main/java/com/example/user_event_analytics/service/UserService.java
+++ src/main/java/com/example/user_event_analytics/service/UserService.java
@@ -5,11 +5,16 @@ import com.example.user_event_analytics.repository.UserRepository;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jakarta.persistence.Query;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
 import jakarta.transaction.Transactional;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -87,6 +92,22 @@ public class UserService {
     }
 
     public List<User> getComplexUsersCriteria(String name, String email) {
-        return null;
+        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+        CriteriaQuery<User> cq = cb.createQuery(User.class);
+        Root<User> user = cq.from(User.class);
+
+        List<Predicate> predicates = new ArrayList<>();
+        if (name != null && !name.isEmpty()) {
+            predicates.add(cb.like(user.get("name"), "%" + name + "%"));
+        }
+        if (email != null && !email.isEmpty()) {
+            predicates.add(cb.like(user.get("email"), "%" + email + "%"));
+        }
+
+        cq.where(predicates.toArray(new Predicate[0]));
+        cq.orderBy(cb.asc(user.get("name")));
+
+        return entityManager.createQuery(cq).getResultList();
     }
+
 }
