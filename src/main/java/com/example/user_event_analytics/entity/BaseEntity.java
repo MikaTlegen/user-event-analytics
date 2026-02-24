cfc6214eb583d51d9d8103ff7849eb10268a1a31
tlege <tlegermuh88@gmail.com>
2026-02-24 13:47:55 +0300

    inheritance of an abstract class

---
+++ src/main/java/com/example/user_event_analytics/entity/BaseEntity.java
@@ -0,0 +1,8 @@
+package com.example.user_event_analytics.entity;
+
+import java.time.LocalDateTime;
+
+public abstract class BaseEntity {
+    public Long id;
+    public LocalDateTime createdAt;
+}
